23931c947545c61ac6853a9bfbefe0705803ff56
LighterLukeCode <lighterluke@gmail.com>
2022-07-06 20:37:41 +0300

    finish blog, start blog-one

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,12 @@ $(function () {
     dots: true,
     arrows: false,
   });
+  $(".article__slider-box").slick({
+    prevArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="./images/blog/slider-arrow-left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="./images/blog/slider-arrow-right.svg"></button>',
+  });
 
   var mixer = mixitup(".gallery__inner", {
     load: {
