990dc2307596a4f7a16c14f514e4961292d5b3cf
LighterLukeCode <lighterluke@gmail.com>
2022-07-07 23:36:11 +0300

    finish responsive

--- app/js/main.js
+++ app/js/main.js
@@ -6,17 +6,51 @@ $(function () {
     $(".rightside-menu").addClass("rightside-menu--close");
   });
 
+  $(".header__btn-menu").on("click", function () {
+    $(".menu").toggleClass("menu--open");
+  });
+
+  if ($(window).width() < 650) {
+    $(".works-path__item--measurements").appendTo($(".works-path__items-box"));
+  }
   $(".top__slider").slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
   });
+
   $(".contact-slider").slick({
     slidesToScroll: 10,
     slidesToShow: 10,
     dots: true,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: { slidesToScroll: 8, slidesToShow: 8 },
+      },
+      {
+        breakpoint: 1511,
+        settings: { slidesToScroll: 6, slidesToShow: 6 },
+      },
+      {
+        breakpoint: 1201,
+        settings: { slidesToScroll: 4, slidesToShow: 4 },
+      },
+      {
+        breakpoint: 841,
+        settings: { slidesToScroll: 3, slidesToShow: 3 },
+      },
+      {
+        breakpoint: 550,
+        settings: { slidesToScroll: 2, slidesToShow: 2 },
+      },
+      {
+        breakpoint: 376,
+        settings: { slidesToScroll: 1, slidesToShow: 1 },
+      },
+    ],
   });
   $(".article__slider-box").slick({
     prevArrow:
